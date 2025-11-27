f4deaa543f4ebd77c23e700f6ea3d8bb6bba9887
Yeff <yefrygf@gmail.com>
2025-11-27 18:23:17 -0500

    chore(api): update payroll changes API URL

--- src/Services/EmployChangesApi.js
+++ src/Services/EmployChangesApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const PAYROLL_CHANGES_URL = "https://maxi-payroll-api-1034515474137.southamerica-east1.run.app/api/v1/form-period/available"
+const PAYROLL_CHANGES_URL = "https://maxi-payroll-api-1069228975395.us-central1.run.app/api/v1/form-period/available"
 
 export const getFormsPeriods = async () => {
 
