5d751e2abcf38016b260b95021a427470b3b4ccb
yefry.gutierrez <yefrygf@gmail.com>
2026-03-25 10:43:05 -0500

    config

--- src/Services/PayrollServices.js
+++ src/Services/PayrollServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const PAYROLL_API_URL = "https://qa-maxi-api-overtimes-1034515474137.southamerica-east1.run.app/api/payroll";
+const PAYROLL_API_URL = "https://maxi-overtimes-api-1069228975395.us-central1.run.app/api/payroll";
 //const PAYROLL_API_URL = "http://localhost:8092/api/payroll";
 
 export const processInfo = async (fileName) => {
